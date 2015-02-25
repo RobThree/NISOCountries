fec7e0cd08da2682aac2dc00111bce5503079c79
RobThree <rob@devcorner.nl>
2015-02-25 12:27:46 +0100

    * Added ISOCountryLookup class

---
+++ NISOCountries.Core/ISOCountryLookup.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace NISOCountries.Core
+{
+    public class ISOCountryLookup<T>
+        where T : IISORecord
+    {
+        private T[] _records;
+        private Dictionary<string, T> _alpha2;
+        private Dictionary<string, T> _alpha3;
+        private Dictionary<string, T> _numeric;
+        private Dictionary<int, T> _numericasint;
+
+        private static Regex _alpha3regex = new Regex("^[A-Z]{3}$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static Regex _alpha2regex = new Regex("^[A-Z]{2}$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static Regex _numericregex = new Regex("^[0-9]{1,3}$", RegexOptions.Compiled);
+
+        public bool IsCaseSensitive { get; private set; }
+
+        public ISOCountryLookup(IISORecordReader<T> recordreader, string source)
+            : this(recordreader.Parse(source)) { }
+
+        public ISOCountryLookup(IISORecordReader<T> recordreader, string source, bool ignoreCase)
+            : this(recordreader.Parse(source), ignoreCase) { }
+
+        public ISOCountryLookup(IEnumerable<T> countries)
+            : this(countries, true) { }
+
+        public ISOCountryLookup(IEnumerable<T> countries, bool ignoreCase)
+        {
+            this.IsCaseSensitive = !ignoreCase;
+            var comparer = this.IsCaseSensitive ? StringComparer.Ordinal : StringComparer.OrdinalIgnoreCase;
+
+            _records = countries.ToArray();
+
+            _alpha2 = _records.Where(r => !string.IsNullOrEmpty(r.Alpha2)).ToDictionary(r => r.Alpha2, comparer);
+            _alpha3 = _records.Where(r => !string.IsNullOrEmpty(r.Alpha3)).ToDictionary(r => r.Alpha3, comparer);
+            _numeric = _records.Where(r => !string.IsNullOrEmpty(r.Numeric)).ToDictionary(r => r.Numeric, comparer);
+            _numericasint = _records.Where(r => !string.IsNullOrEmpty(r.Numeric)).ToDictionary(r => int.Parse(r.Numeric));
+        }
+
+        public bool TryGetByAlpha2(string alpha2, out T result)
+        {
+            return _alpha2.TryGetValue(alpha2, out result);
+        }
+
+        public bool TryGetByAlpha3(string alpha3, out T result)
+        {
+            return _alpha3.TryGetValue(alpha3, out result);
+        }
+
+        public bool TryGetByNumeric(string numeric, out T result)
+        {
+            return _numeric.TryGetValue(numeric, out result);
+        }
+
+        public bool TryGetByNumeric(int numeric, out T result)
+        {
+            return _numericasint.TryGetValue(numeric, out result);
+        }
+
+        public T GetByAlpha2(string alpha2)
+        {
+            return _alpha2[alpha2];
+        }
+
+        public T GetByAlpha3(string alpha3)
+        {
+            return _alpha3[alpha3];
+        }
+
+        public T GetByNumeric(string numeric)
+        {
+            return _numeric[numeric];
+        }
+
+        public T GetByNumeric(int numeric)
+        {
+            return _numericasint[numeric];
+        }
+
+        public T GetByAlpha(string alpha)
+        {
+            if (alpha == null)
+                throw new ArgumentNullException();
+
+            if (_alpha3regex.IsMatch(alpha))
+                return GetByAlpha3(alpha);
+            if (_alpha2regex.IsMatch(alpha))
+                return GetByAlpha2(alpha);
+
+            throw new ArgumentException("Invalid length for alpha argument (must be either 2 or 3)", alpha);
+        }
+
+        public bool TryGetByAlpha(string alpha, out T result)
+        {
+            if (alpha == null)
+                throw new ArgumentNullException();
+
+            if (_alpha3regex.IsMatch(alpha))
+                return TryGetByAlpha3(alpha, out result);
+            if (_alpha2regex.IsMatch(alpha))
+                return TryGetByAlpha2(alpha, out result);
+
+            result = default(T);
+            return false;
+        }
+
+        public T this[string code]
+        {
+            get { return this.Get(code); }
+        }
+
+        public T Get(string code)
+        {
+            if (code == null)
+                throw new ArgumentNullException();
+
+            T result;
+            if (TryGet(code, out result))
+                return result;
+
+            throw new KeyNotFoundException();
+        }
+
+        public bool TryGet(string code, out T result)
+        {
+            if (code == null)
+                throw new ArgumentNullException();
+
+            if (TryGetByAlpha(code, out result))
+                return true;
+
+            if (_numericregex.IsMatch(code))
+                return TryGetByNumeric(code, out result);
+            
+            return false;
+        }
+
+        public IEnumerable<T> Values { get { return _records; } }
+    }
+}
--- NISOCountries.Tests/UnitTest1.cs
+++ NISOCountries.Tests/UnitTest1.cs
@@ -25,12 +25,46 @@ namespace NISOCountries.Tests
             var r = new RipeReader(s).Parse(@"Test\fixtures\ripe_testfile.txt");
             var g = new GeonamesReader(s).Parse(@"Test\fixtures\geonames_testfile.txt");
 
-            var q = r.Cast<IISORecord>()
-                .Union(w.Cast<IISORecord>())
-                .Union(g.Cast<IISORecord>())
-                .ToArray();
+            //var q = r.Cast<IISORecord>()
+            //    .Union(w.Cast<IISORecord>())
+            //    .Union(g.Cast<IISORecord>())
+            //    .ToArray();
+
+            var x1 = new ISOCountryLookup<IISORecord>(w);
+            var x2 = new ISOCountryLookup<IISORecord>(r);
+            var x3 = new ISOCountryLookup<IISORecord>(g);
+
+            var x4 = new ISOCountryLookup<WikipediaRecord>(new WikipediaReader(s), @"Test\fixtures\wikipedia_testfile.htm");
+            var x5 = new ISOCountryLookup<RipeRecord>(new RipeReader(s), @"Test\fixtures\ripe_testfile.txt");
+            var x6 = new ISOCountryLookup<GeonamesRecord>(new GeonamesReader(s), @"Test\fixtures\geonames_testfile.txt");
+
+            var r1 = x1.GetByAlpha2("nl");
+            var r2 = x1.GetByAlpha2("NL");
+
+            var r3 = x2.GetByNumeric("036");
+            var r4 = x2.GetByNumeric(36);
+
+            var r5 = x3.GetByAlpha3("NLD");
+
+            var q1 = x1.Get("NL");
+            var q2 = x1.Get("NLD");
+            var q3 = x1.Get("528");
+
+            var w1 = x1["NL"];
+            var w2 = x1["NLD"];
+            var w3 = x1["528"];
+
+            IISORecord result1;
+            IISORecord result2;
+            IISORecord result3;
+            var q4 = x1.TryGet("NL", out result1);
+            var q5 = x1.TryGet("NLD", out result2);
+            var q6 = x1.TryGet("528", out result3);
+
+            Assert.ReferenceEquals(r1, r2);
+            Assert.ReferenceEquals(r3, r4);
         }
 
-        //TODO: Unittests for readers, valuenormalizers, sourceproviders
+        //TODO: Unittests for readers, valuenormalizers, sourceproviders, ISOCountryLookup etc.
     }
 }
