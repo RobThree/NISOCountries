7153042f92aa883fc4d5a53af42831fd3741c404
RobThree <rob@devcorner.nl>
2015-02-25 02:34:45 +0100

    * Added nuspec files * Switched to .Net 4.0-client where possible

---
+++ buildnugetpackages.cmd
@@ -0,0 +1,4 @@
+.nuget\nuget pack NISOCountries.Core\NISOCountries.Core.nuspec
+.nuget\nuget pack NISOCountries.GeoNames\NISOCountries.GeoNames.nuspec
+.nuget\nuget pack NISOCountries.Ripe\NISOCountries.Ripe.nuspec
+.nuget\nuget pack NISOCountries.Wikipedia\NISOCountries.Wikipedia.nuspec
