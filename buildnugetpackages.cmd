3a6649a01ac7155790d9f9523675a4f6de6c7d47
RobThree <rob@devcorner.nl>
2018-02-25 01:14:48 +0100

    * Add .Net Core and .Net Standard support

--- buildnugetpackages.cmd
+++
@@ -1,6 +0,0 @@
-.nuget\nuget pack NISOCountries.Core\NISOCountries.Core.nuspec
-.nuget\nuget pack NISOCountries.GeoNames\NISOCountries.GeoNames.nuspec
-.nuget\nuget pack NISOCountries.Ripe\NISOCountries.Ripe.nuspec
-.nuget\nuget pack NISOCountries.Wikipedia\NISOCountries.Wikipedia.nuspec
-.nuget\nuget pack NISOCountries.Wikipedia.CSQ\NISOCountries.Wikipedia.CSQ.nuspec
-.nuget\nuget pack NISOCountries.Wikipedia.HAP\NISOCountries.Wikipedia.HAP.nuspec
